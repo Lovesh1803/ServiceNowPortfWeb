c25ac6a1096a71effec237d954b4f59a7f33d402
loveshsingh <loveshsinghsuperfine@gmail.com>
2025-05-13 22:17:22 +0530

    Fix url in ALLOWED_HOSTS

--- servicenowport/servicenowport/settings.py
+++ servicenowport/servicenowport/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    'https://servicenowportfweb.onrender.com/',    
     'localhost',
     '127.0.0.1',
+    'servicenowportfweb.onrender.com/',    
 ]
 
 
