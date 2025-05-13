0f202e060370042e14e051fa54ecf778730df48f
loveshsingh <loveshsinghsuperfine@gmail.com>
2025-05-13 22:38:36 +0530

    Fix DATABASE_URL parsing

--- servicenowport/servicenowport/settings.py
+++ servicenowport/servicenowport/settings.py
@@ -87,9 +87,12 @@ WSGI_APPLICATION = 'servicenowport.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# Pull DATABASE_URL (must exist in env)
+DATABASE_URL = os.environ['DATABASE_URL']
+
 DATABASES = {
     'default': dj_database_url.parse(
-    os.environ.get('DATABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
+    DATABASE_URL,
     conn_max_age=600,
     ssl_require=True
   )
