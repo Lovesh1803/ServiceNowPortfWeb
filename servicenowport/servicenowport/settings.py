df2fde2014c6af516d3acce2f999dce0e8117605
loveshsingh <loveshsinghsuperfine@gmail.com>
2025-05-13 22:28:23 +0530

    Fix: Build config.

--- servicenowport/servicenowport/settings.py
+++ servicenowport/servicenowport/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'servicenowport.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-    default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+    default=os.environ.get('DATABASE_URL'),
     conn_max_age=600,
     ssl_require=True
   )
