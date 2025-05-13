392d2a55fa6708ee1cda1fc9d9b221514160eff1
loveshsingh <loveshsinghsuperfine@gmail.com>
2025-05-13 22:33:41 +0530

    Fix DATABASES parsing for Render

--- servicenowport/servicenowport/settings.py
+++ servicenowport/servicenowport/settings.py
@@ -88,8 +88,8 @@ WSGI_APPLICATION = 'servicenowport.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-    default=os.environ.get('DATABASE_URL'),
+    'default': dj_database_url.parse(
+    os.environ.get('DATABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
     conn_max_age=600,
     ssl_require=True
   )
