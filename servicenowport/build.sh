ecce8d3af31a1cb03bc2d3a356c2566317e6f60f
loveshsingh <loveshsinghsuperfine@gmail.com>
2025-05-13 23:14:36 +0530

    Add seeding data config.

--- servicenowport/build.sh
+++ servicenowport/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 pip install -r requirements.txt
+python manage.py createsuperuser --no-input
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --no-input
+python manage.py seed_all
